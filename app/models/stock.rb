e144c6f5eecf036b374e3daaeba567ee84e0abe9
Chris <390074138@qq.com>
2020-05-14 22:34:40 -0700

    add list view for stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
       publishable_token: Rails.application.credentials.iex_client[:publishable_token],
@@ -10,6 +14,10 @@ class Stock < ApplicationRecord
     rescue => exception
       nil
     end
+  end
+
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 
   def self.get_logo(ticker_symbol)
