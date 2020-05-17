5167d115395983817bd214f7adb1c5e9d4577ed5
Chris <390074138@qq.com>
2020-05-16 18:10:17 -0700

    add friend tracking and view

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,33 @@ class User < ApplicationRecord
     return "#{first_name}  #{last_name}" if first_name or last_name
     "Anonymous"
   end
+
+  def self.search(param)
+    param.strip!
+    result = (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+    return nil unless result
+    result
+  end
+
+  def self.first_name_matches(param)
+    matches("first_name", param)
+  end
+  def self.last_name_matches(param)
+    matches("last_name", param)
+  end
+  def self.email_matches(param)
+    matches("email", param)
+  end
+
+  def self.matches(field_name, param)
+    where("#{field_name} like ?", "%#{param}%")
+  end
+
+  def except_current_user(friends)
+    friends.reject { |friend| friend.id == self.id }
+  end
+
+  def not_friends_with?(id_of_friend)
+    !self.friends.where(id: id_of_friend).exists?
+  end
 end
